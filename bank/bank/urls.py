ec138ab04e48c7f68abf634d62f8a1329098469b
Riku Kalinen <riku@kalinen.net>
2020-12-25 21:36:14 +0200

    start over with bank

--- bank/bank/urls.py
+++ bank/bank/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+
+from django.contrib import admin
+from django.urls import include,path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('bank/', views.bank, name='bank'),
+    path('admin/', admin.site.urls),
 ]
